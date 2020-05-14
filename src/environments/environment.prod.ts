03ec5bf640a98fa28089438224f0615e3f1de1f1
Diego <jose.sarmiento@mdp.com.pe>
2020-05-14 08:02:37 -0500

    as

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   "url": "http://159.89.121.195:8080/appPasarelaMultimarcaAPI",
-  'url_limpio': "http://159.89.121.195:8080/"
+  'url_limpio': "http://159.89.121.195:8080/AppTodosPasarelaMultimarcaApi"
   //"url": "http://siddev.site:8080/pasarela"
 };
